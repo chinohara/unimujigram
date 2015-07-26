5e1dbf19b414722d3bf90a77ce6deb68d782347a
chinohara <snhr.not@gmail.com>
2015-07-26 17:07:36 +0900

    test commit

---
+++ src/main/java/com/example/models/TestDto.java
@@ -0,0 +1,16 @@
+package com.example.models;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class TestDto {
+	private String str;
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+}
--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,9 +1,13 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
+import com.example.models.TestDto;
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +18,20 @@ public class TimeService {
     @GET
     public Time get() {
         return new Time();
+    }
+    
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) {
+        return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+    }
+    
+    @GET
+    @Path("/hello/{param}")
+    public TestDto getHello(@PathParam("param") String param) {
+    	TestDto dto = new TestDto();
+    	dto.setStr("Hello!" + param);
+        return dto;
     }
 
 }
